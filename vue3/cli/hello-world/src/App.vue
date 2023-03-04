5a51cf1d6beb31943d0336881dbba4766985044d
leninrkb <rkb.lenin@gmail.com>
2023-03-04 00:32:28 -0500

    form en appvue

--- vue3/cli/hello-world/src/App.vue
+++ vue3/cli/hello-world/src/App.vue
@@ -2,29 +2,53 @@
   <div>
     <h1>Hola mundo</h1>
     <button class="btn blue" @click='saludar()'>
-      boton
+      saludar
       <span class="material-icons">save</span>
     </button>
-    <div class="col m12 card-panel">
-      <div class="col m4">
-        <label>nombre</label>
-        <input type="text" v-model="nombre">
-      </div>
-      <div class="col m4">
-        <label>apellido</label>
-        <input type="text" v-model="apellido">
-      </div>
-      <div class="row">
+    <div class="container">
+      <div class="col m12 card-panel grey lighten-5">
         <div class="col m4">
-          <label>edad</label>
-          <input type="number" v-model="edad" max="99" min="10">
+          <label>nombre</label>
+          <input type="text" v-model="nombre">
         </div>
         <div class="col m4">
-          <label>estado civil</label>
-          <select>
-            <option disabled selected>Selecciona tu estado</option>
-            <option v-for="(estado, index) in estados_civiles" :key="index" value="estado">{{estado}}</option>
-          </select>
+          <label>apellido</label>
+          <input type="text" v-model="apellido">
+        </div>
+        <div class="col m4">
+          <label>email</label>
+          <input type="email" v-model="email">
+        </div>
+        <div class="row">
+          <div class="col m4">
+            <label>edad</label>
+            <input type="number" v-model="edad" max="99" min="10">
+          </div>
+          <div class="col m4">
+            <label>estado civil</label>
+            <select>
+              <option disabled selected>Selecciona tu estado</option>
+              <option v-for="(estado, index) in estados_civiles" :key="index" value="estado">{{ estado }}</option>
+            </select>
+          </div>
+          <div class="col m4">
+            <label>
+              <input type="checkbox" class="filled-in" v-model="suscribirse" />
+              <span>Suscribirse</span>
+            </label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col m4">
+            <label>Pasatiempo</label>
+            <input type="text" v-model="pasatiempo">
+            <button class="btn" @click="limpiarPasatiempo()">Limpiar <span class="material-icons">clear</span> </button>
+            <button class="btn" @click="agregarPasatiempo()">Agregar <span class="material-icons">create</span> </button>
+          </div>
+          <div class="col m4">
+            <label>Mis pasatiempos</label>
+            <li v-for="(pasatiempo, index) in pasatiempos" :key="index">{{pasatiempo}} <i class="material-icons" @click="quitarPasatiempo(index)">clear</i> </li>
+          </div>
         </div>
       </div>
     </div>
@@ -47,8 +71,10 @@ export default {
     return {
       nombre: '',
       apellido: '',
+      email:'',
       edad: 10,
       estado_civil: 'asd',
+      suscribirse: false,
       saludo: 'hola mundo desde vue',
       estados_civiles: [
         'casado/a',
@@ -56,6 +82,8 @@ export default {
         'divorciado/a',
         'viudo/a',
       ],
+      pasatiempos:[],
+      pasatiempo:'',
       instances: []
     }
   },
@@ -65,6 +93,29 @@ export default {
         html: this.saludo + ' ' + this.nombre
       })
       console.log('saludo desde app.vue');
+    },
+    agregarPasatiempo(){
+      this.pasatiempo = this.pasatiempo.trim();
+      if(this.pasatiempo == ''){
+        M.toast({
+          html:'no se admite espacios en blanco'
+        })
+        return;
+      }
+      if(this.pasatiempo.length > 20){
+        M.toast({
+          html:'no se admite cadenas > 20'
+        })
+        return;
+      }
+      this.pasatiempos.push(this.pasatiempo)
+      this.pasatiempo=''
+    },
+    quitarPasatiempo(index){
+      this.pasatiempos.splice(index,1);
+    },
+    limpiarPasatiempo(){
+      this.pasatiempo=''
     }
   }
 }
