f3b82704bdf19d20723654019d1c755c8af1e660
leninrkb <rkb.lenin@gmail.com>
2023-03-03 23:37:51 -0500

    update appvue

--- vue3/cli/hello-world/src/App.vue
+++ vue3/cli/hello-world/src/App.vue
@@ -5,6 +5,29 @@
       boton
       <span class="material-icons">save</span>
     </button>
+    <div class="col m12 card-panel">
+      <div class="col m4">
+        <label>nombre</label>
+        <input type="text" v-model="nombre">
+      </div>
+      <div class="col m4">
+        <label>apellido</label>
+        <input type="text" v-model="apellido">
+      </div>
+      <div class="row">
+        <div class="col m4">
+          <label>edad</label>
+          <input type="number" v-model="edad" max="99" min="10">
+        </div>
+        <div class="col m4">
+          <label>estado civil</label>
+          <select>
+            <option disabled selected>Selecciona tu estado</option>
+            <option v-for="(estado, index) in estados_civiles" :key="index" value="estado">{{estado}}</option>
+          </select>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,20 +35,34 @@
 import M from 'materialize-css'
 export default {
   name: 'App',
-  mounted(){
-    this.axios.get('https://leninrkb.com').then(() => {
+  mounted() {
+    this.estado_civil = this.estados_civiles[0];
+    // this.axios.get('https://leninrkb.com').then(() => {
 
-    });
+    // });
+    var elems = document.querySelectorAll('select');
+    this.instances = M.FormSelect.init(elems, null);
   },
-  data(){
-    return{
-      saludo:'hola mundo desde vue'
+  data() {
+    return {
+      nombre: '',
+      apellido: '',
+      edad: 10,
+      estado_civil: 'asd',
+      saludo: 'hola mundo desde vue',
+      estados_civiles: [
+        'casado/a',
+        'soltero/a',
+        'divorciado/a',
+        'viudo/a',
+      ],
+      instances: []
     }
   },
-  methods:{
-    saludar(){
+  methods: {
+    saludar() {
       M.toast({
-        html:this.saludo
+        html: this.saludo + ' ' + this.nombre
       })
       console.log('saludo desde app.vue');
     }
