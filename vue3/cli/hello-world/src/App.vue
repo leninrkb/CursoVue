594a857ef8cb68c8f7efe5d170bc011d055cf39b
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-06 17:24:21 -0500

    arreglo navbar

--- vue3/cli/hello-world/src/App.vue
+++ vue3/cli/hello-world/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container">
-    <div class="row">
-        <nav class="cyan" >
+  <div id="margen-nav" class="blue-grey darken-3">
+    <nav class="cyan darken-1">
+      <div class="row">
         <div class="col s12 m4">
           <h5>Mi portafolio</h5>
         </div>
@@ -12,14 +12,23 @@
             <li class="tab"><a href="#proyectos">Proyectos</a></li>
           </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
+  </div>
+
+  <div class="container">
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+    <div class="row"></div>
+
     <div class="row">
-      <div id="sobremi" class="col s12">
+      <div id="sobremi">
         <PersonalView></PersonalView>
       </div>
-      <div id="experiencia" class="col s12">Test 2</div>
-      <div id="proyectos" class="col s12">Test 2</div>
+      <div id="experiencia">Test 2</div>
+      <div id="proyectos">Test 2</div>
     </div>
   </div>
 </template>
@@ -49,14 +58,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 body {
-  background-color: #2c3e50;
+  /*blue-grey darken-3*/
+  background-color: #37474f;
 }
 
-nav{
+nav {
   border-radius: 10px;
+}
+
+#margen-nav {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  padding: 15px;
+  margin-bottom: 155px;
 }
 </style>
