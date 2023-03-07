6a39f5dcedca493e00c489e0838a4a8a51341a86
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-06 19:08:29 -0500

    formato y preparo grid

--- vue3/cli/hello-world/src/views/ExperienciaView.vue
+++ vue3/cli/hello-world/src/views/ExperienciaView.vue
@@ -1,10 +1,39 @@
 <template>
-    <div class="row  red">
-        <div class="col s12 m7 cyan">
-            col1
+    <div class="row">
+        <div class="row">
+            <div class="col s12 m7">
+                <div class="card cyan darken-3 redondeado">
+                    <div class="card-content justificado white-text">
+                        <span class="card-title">titulo</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid rem doloremque corrupti! Architecto rerum sequi debitis! Asperiores soluta dolorem, qui, corporis temporibus voluptate exercitationem officiis ratione explicabo blanditiis sed officia.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m5">
+                <div class="card teal darken-3 redondeado">
+                    <div class="card-content justificado white-text">
+                        mas info
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col s12 m7 offset-m5 cyan">
-            col2
+
+        <div class="row">
+            <div class="col s12 m5">
+                <div class="card teal darken-3 redondeado">
+                    <div class="card-content justificado white-text">
+                        mas info
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m7">
+                <div class="card cyan darken-3 redondeado">
+                    <div class="card-content justificado white-text">
+                        <span class="card-title">titulo</span>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, a rerum. Autem quos ut atque quis laborum quae voluptas qui. Numquam aliquid, possimus non quisquam commodi quasi autem? Dolore, perspiciatis?</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
