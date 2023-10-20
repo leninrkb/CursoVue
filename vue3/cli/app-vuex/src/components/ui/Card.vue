e2e7c2ca9907579be72fc67b99d09170a63ab187
Lenin Acosta <rkb.lenin@gmail.com>
2023-10-19 22:12:10 -0500

    some changes

--- vue3/cli/app-vuex/src/components/ui/Card.vue
+++ vue3/cli/app-vuex/src/components/ui/Card.vue
@@ -1,6 +1,5 @@
 <template lang="en">
     <div>
-        <label class="font-medium text-lg capitalize" for="card">{{card.titulo}}</label>
         <div id="card">
             <div class="text-gray-50 border-2 border-gray-500 bg-gray-400 rounded-lg p-2">
                <slot></slot> 
@@ -12,8 +11,5 @@
 <script>
 export default {
   name: "Card",
-  props: {
-    card: Object,
-  },
 };
 </script>
--- vue3/cli/app-vuex/src/views/HomeView.vue
+++ vue3/cli/app-vuex/src/views/HomeView.vue
@@ -1,23 +1,27 @@
 <template>
-  <CardV1 :card="card">
-    <div class="">
-      hola mundo
+  <div class="py-1">
+    <div>
+      <Card>
+        barra de navegacion
+      </Card>
     </div>
-  </CardV1>
+    <div class="mt-1">
+      <Card>
+        renderizo segun de al boton
+      </Card>
+    </div>
+  </div>
+  
 </template>
 
 <script>
 import Card from '@/components/ui/Card.vue';
 import CardV1 from '@/components/ui/CardV1.vue';
+
 export default {
   name: 'HomeView',
   components: {
     Card, CardV1
-  },
-  data() {
-    return {
-      card: { titulo: "Este es un card" }
-    }
   },
 }
 </script>
