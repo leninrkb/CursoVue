2e6667b560cf28c150b43e4fe02860ec650e12dd
Lenin Acosta <rkb.lenin@gmail.com>
2024-02-03 14:01:00 -0500

    teachers done

---
+++ 2024/teachers/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <TeacherForm></TeacherForm>
+</template>
+
+<script setup>
+import TeacherForm from "@/components/TeacherForm.vue";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ 2024/teachers/src/components/TeacherForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <h1>Teachers registry</h1>
+    <div>
+        <form @submit.prevent="submitHandler">
+            name: <input type="text" v-model="teacher.name">
+            <br>
+            surname: <input type="text" v-model="teacher.surname">
+            <br>
+            docs: <input type="checkbox" v-model="teacher.docs">
+            <br>
+            <input type="submit">
+        </form>
+    </div>
+    <hr>
+    <div>
+        <h4>Teacher list</h4>
+        <table>
+            <thead>
+                <tr>
+                    <th>name</th>
+                    <th>surname</th>
+                    <th>docs</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="teacher, index in teachers" :key="index">
+                    <td>{{teacher.name}}</td>
+                    <td>{{teacher.surname}}</td>
+                    <td>{{teacher.docs}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let teacher = ref({
+    name:'',
+    surname:'',
+    docs:false
+})
+let teachers = ref([])
+const addTeacher = () => {
+    teachers.value.push({
+        name: teacher.value.name,
+        surname: teacher.value.surname,
+        docs: teacher.value.docs,
+    })
+}
+const clearFields = () => {
+    teacher.value.name = ''
+    teacher.value.surname = ''
+    teacher.value.docs = false
+}
+const submitHandler = () => {
+    addTeacher()
+    clearFields()
+    console.log('submit');
+}
+</script>
