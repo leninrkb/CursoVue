671156f342e458d9288b0c00f7a112b6d531554a
Lenin Acosta <rkb.lenin@gmail.com>
2024-02-08 11:20:41 -0500

    post api

---
+++ 2024/services/src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul>
+        <ol v-for="data, index in posts" :key="index">
+            <li>id: {{data.id}}</li>
+            <li>user id: {{data.userId}}</li>
+            <li>title: {{data.title}}</li>
+            <li>body: {{data.body}}</li>
+            <br>
+        </ol>
+    </ul>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import PostService from '@/services/PostService.js'
+
+const service = new PostService()
+let posts = service.getPosts()
+
+onMounted(async () => {
+    await service.fetchAll()
+})
+</script>
+
+<style scoped></style>
