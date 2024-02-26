bee83aa13c23a31a407b2dca640d7133bd96f4e8
Lenin Acosta <rkb.lenin@gmail.com>
2024-02-25 19:47:01 -0500

    cookies methods

--- 2024/cookies-sessions/src/main.js
+++ 2024/cookies-sessions/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueCookies } from 'vue-cookies'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueCookies, {
+    expires: '1d'
+}).mount('#app')
+
+// $cookies.set('auth',1000)
+// $cookies.get('auth')
+// $cookies.isKey('auth')
+// $cookies.keys()
